00a1835e8409995560c42d9f999ee7f1b2bbb395
Taiyo0004 <adityanaidu1014@gmail.com>
2025-09-08 20:31:04 +0530

    Just to try if this works or not

--- can_communication.py
+++ can_communication.py
@@ -2,23 +2,39 @@ from canlib import Frame, canlib
 
 
 def open_channel(channel):
+    "Open a new channel, set bitrate 1Mbit/s and go bus on."
     ch = canlib.openChannel(channel, canlib.canOPEN_ACCEPT_VIRTUAL)
     ch.setBusOutputControl(canlib.canDRIVER_NORMAL)
-    ch.setBusParams(canlib.canBITRATE_500K)
+    ch.setBusParams(canlib.canBITRATE_1M)
     ch.busOn()
     return ch
 
 
-# Open two channels
-ch_send = open_channel(channel=0)
+def close_channel(ch):
+    "Go bus off and close channel."
+    ch.busOff()
+    ch.close()
 
-# Setup both channels
-ch_send.setBusOutputControl(canlib.canDRIVER_NORMAL)
-ch_send.busOn()
-while True:
-    command_int = input("Enter the command: ")
-    command = int(command_int, 0)
-    command_array = [command, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
-    # Send on one channel
-    frame = Frame(id_=4, data=bytearray(command_array))
-    ch_send.write(frame)
+
+def send_receive_raw(ch0, ch1, message):
+    # send a CAN frame without using kvadblib
+    frame = Frame(id_=4, data=bytearray(message))
+    print("Sending frame: % s" % frame)
+    ch0.write(frame)
+
+    # read message back
+    print("Receiving frame: % s" % str(ch1.read()))
+
+
+ch0 = open_channel(1)
+ch1 = open_channel(0)
+
+message = "0x35, 0x00, 0x00, 0x00, 0x00, 0x00 0x00, 0x00"
+send_receive_raw(
+    ch0,
+    ch1,
+    message,
+)
+
+close_channel(ch0)
+close_channel(ch1)
